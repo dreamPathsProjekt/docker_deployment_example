e7a633cdc8fac7e5990d2d8f1e891708b98f39ec
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2018-01-24 00:10:34 +0200

    Moved builds mariadb, wordpress, pma to OBS

--- permissions.sh
+++ permissions.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 chmod a+x ./caddy/build-script.sh && \
-chmod a+x ./wordpress/build-script.sh && \
-chmod a+x ./mariadb/build-script.sh && \
-chmod a+x ./pma/build-script.sh && \
+# chmod a+x ./wordpress/build-script.sh && \
+# chmod a+x ./mariadb/build-script.sh && \
+# chmod a+x ./pma/build-script.sh && \
+
+cd ./caddy && \
+./build-script $1 && \
+cd ../
 
 # docker stack deploy --compose-file ./development/wp_mariadb_admin.yml wp
 
