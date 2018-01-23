539b2b8fa371624905b519d599c310145825878a
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2018-01-23 23:32:37 +0200

    Modify builds to version 0.2

--- pma/docker-entry.sh
+++ pma/docker-entry.sh
@@ -4,4 +4,5 @@ export PMA_USER=$(cat /run/secrets/pma_user)
 export PMA_PASSWORD=$(cat /run/secrets/pma_pass)
 export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_pass)
 
-exec /run.sh
+set -- /run.sh "$@"
+exec "$@"
--- wordpress/docker-entry.sh
+++ wordpress/docker-entry.sh
@@ -2,4 +2,5 @@
 
 export WORDPRESS_DB_PASSWORD_FILE=$(cat /run/secrets/wp_db_pass)
 
-exec /usr/local/bin/docker-entrypoint.sh
+set -- /usr/local/bin/docker-entrypoint.sh "$@"
+exec "$@"
