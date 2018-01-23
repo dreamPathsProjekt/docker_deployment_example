f802e50f6090bc74f5e647ac1ebf1bb39262946f
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2018-01-23 22:31:52 +0200

    Fix root db password env

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod a+x ./caddy/build-script.sh && \
-caddy/build-script.sh $1 && \
+./caddy/build-script.sh $1 && \
 docker stack deploy --compose-file ./development/wp_mariadb_admin.yml wp
 
 exit 0
