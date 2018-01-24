67f545d9d7031f7c3cf3949c799d3f0cce1dbd9e
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2018-01-24 19:32:32 +0200

    Fix registry domain name

--- caddy/build-script.sh
+++ caddy/build-script.sh
@@ -2,9 +2,9 @@
 
 chmod a+x `pwd`/caddy/docker-entry.sh && \
 docker build -t caddy `pwd`/caddy/. && \
-docker tag caddy registry.greece:5001/caddy:$1 && \
-docker tag caddy registry.greece:5001/caddy:latest && \
-docker push registry.greece:5001/caddy:$1 && \
-docker push registry.greece:5001/caddy:latest
+docker tag caddy registry.dream:5001/caddy:$1 && \
+docker tag caddy registry.dream:5001/caddy:latest && \
+docker push registry.dream:5001/caddy:$1 && \
+docker push registry.dream:5001/caddy:latest
 
 exit 0
