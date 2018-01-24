4656a7ffe1c2179953bee00566f0c7d4d133af1b
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2018-01-24 21:32:58 +0200

    Minor fix on docker-exporter build-script

--- docker-exporter/build-script.sh
+++ docker-exporter/build-script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-chmod a+x `pwd`/docker-exporter/docker-entry.sh && \
 docker build -t docker-exporter `pwd`/docker-exporter/. && \
 docker tag docker-exporter registry.dream:5001/docker-exporter:$1 && \
 docker tag docker-exporter registry.dream:5001/docker-exporter:latest && \
